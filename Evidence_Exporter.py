7afb02cdaa58236043f2c001ab5c0f75d22a183c
Jan Kaiser <2159464+jankais3r@users.noreply.github.com>
2022-02-25 16:41:05 +0100

    v0.3

--- Evidence_Exporter.py
+++ Evidence_Exporter.py
@@ -23,16 +23,17 @@ import xry
 import io
 import os
 import csv
+import sys
 import time
 import zipfile
 import datetime
 
-__contact__ = "https://github.com/jankais3r"
-__version__ = "0.2"
+__contact__ = "https://github.com/jankais3r/XRY-Evidence-Extractor"
+__version__ = "0.3"
 __description__ = "Creates a ZIP archive with logical evidence that can be parsed by other forensic tools"
 
-
-def monkeyWritecheck(self, zinfo): # ʕ ᵔᴥᵔ ʔ monkey-patching a zipfile function that usually just warns about duplicate files to skip them entirely
+# ʕᵔᴥᵔʔ Monkey-patching a zipfile function that usually just warns about duplicate files to skip them entirely.
+def monkeyWritecheck(self, zinfo):
 		"""Check for errors before writing a file to the archive."""
 		if zinfo.filename in self.NameToInfo:
 			#import warnings
@@ -55,9 +56,79 @@ def monkeyWritecheck(self, zinfo): # ʕ ᵔᴥᵔ ʔ monkey-patching a zipfile f
 				raise LargeZipFile(requires_zip64 + ' would require ZIP64 extensions')
 zipfile.ZipFile._writecheck = monkeyWritecheck
 
+# We need this to calculate the remaining RAM. Source: https://stackoverflow.com/a/31546725
+from ctypes import Structure, c_int32, c_uint64, sizeof, byref, windll
+class MemoryStatusEx(Structure):
+	_fields_ = [
+		('length', c_int32),
+		('memoryLoad', c_int32),
+		('totalPhys', c_uint64),
+		('availPhys', c_uint64),
+		('totalPageFile', c_uint64),
+		('availPageFile', c_uint64),
+		('totalVirtual', c_uint64),
+		('availVirtual', c_uint64),
+		('availExtendedVirtual', c_uint64)]
+	def __init__(self):
+		self.length = sizeof(self)
+
+def writeToFile():
+	global logText, fileBuffer
+	with zipfile.ZipFile(os.path.join(outputDir, 'Extracted_' + time.strftime('%Y-%m-%d %H_%M_%S', time.gmtime(startTime)) + '.zip'), mode = 'a') as zf:
+		for f in fileBuffer:
+			itemName = f[0]
+			itemPath = f[1]
+			itemXRYModified = f[2]
+			itemSize = f[4]
+			
+			if len(itemName) > 0 and len(itemPath) > 0:
+				try:
+					itemXRYModified = str(itemXRYModified[0])
+					itemZIPModified = itemXRYModified[:itemXRYModified.find(' UTC')]
+					if itemZIPModified[1] == '/':
+						itemZIPModified = '0' + itemZIPModified
+					if itemZIPModified[4] == '/':
+						itemZIPModified = itemZIPModified[:3] + '0' + itemZIPModified[3:]
+					if itemZIPModified[12] == ':':
+						itemZIPModified = itemZIPModified[:11] + '0' + itemZIPModified[11:]
+					itemZIPModified = time.localtime(datetime.datetime.strptime(itemZIPModified, '%m/%d/%Y %I:%M:%S %p').timestamp())
+					status = 'OK'
+					reason = 'OK'
+					info = zipfile.ZipInfo(str(itemPath[0]) + str(itemName[0]), date_time = itemZIPModified)
+					zf.writestr(info, f[3].getvalue())
+					itemZIPModified = datetime.datetime.fromtimestamp(time.mktime(itemZIPModified)).strftime('%Y-%m-%d %H:%M:%S')
+				except ValueError: # Triggers on the monkey-patched function on duplicate item entry.
+					status = 'Skipped file'
+					reason = 'Extracted child item (Extracted archive item)'
+					itemXRYModified = ''
+					itemZIPModified = ''
+				except Exception as e: # Triggers on all other items with no Date Modified, which is an indication of XRY synthesised item.
+					#print(e) # If you are getting weird results (e.g., 0 files exported), uncomment this print() to see why the files fail to be exported.
+					status = 'Skipped file'
+					reason = 'Extracted child item (Synthesised XRY item)'
+					itemXRYModified = ''
+					itemZIPModified = ''
+			else: # We are not interested in "files" that do not have a Path or File Name.
+				status = 'Skipped file'
+				reason = 'Missing Path or Name'
+				itemZIPModified = ''
+				try:
+					itemXRYModified = str(itemXRYModified[0])
+				except:
+					itemXRYModified = ''
+			try:
+				itemPath = str(itemPath[0])
+			except:
+				itemPath = ''
+			try:
+				itemName = str(itemName[0])
+			except:
+				itemName = ''
+			logText += '"' + status + '","' + reason + '","' + itemPath + '","' + itemName + '","' + itemXRYModified + '","' + itemZIPModified + '","' + itemSize + '"\n'
+	fileBuffer = []
 
 def parseRecursive(image, node, item):
-	global logText
+	global logText, fileBuffer
 	try:
 		items = image.get_children(item[0])
 	except:
@@ -69,71 +140,35 @@ def parseRecursive(image, node, item):
 		itemData = image.get_properties_of_type(items[i], xry.proptypes.raw_data)
 		itemXRYModified = image.get_properties_of_type(items[i], xry.proptypes.modified)
 		
-		if len(itemName) > 0 and len(itemPath) > 0:
-			try:
-				itemXRYModified = str(itemXRYModified[0])
-				itemZIPModified = itemXRYModified[:itemXRYModified.find(' UTC')]
-				if itemZIPModified[1] == '/':
-					itemZIPModified = '0' + itemZIPModified
-				if itemZIPModified[4] == '/':
-					itemZIPModified = itemZIPModified[:3] + '0' + itemZIPModified[3:]
-				if itemZIPModified[12] == ':':
-					itemZIPModified = itemZIPModified[:11] + '0' + itemZIPModified[11:]
-				itemZIPModified = time.localtime(datetime.datetime.strptime(itemZIPModified, '%m/%d/%Y %I:%M:%S %p').timestamp())
-				status = 'OK'
-				reason = 'OK'
-				
-				mem = image.create_memifc(itemData[0])
-				fakeFile = io.BytesIO(mem[:])
-				with zipfile.ZipFile(os.path.join(outputDir, 'Extracted_' + time.strftime('%Y-%m-%d %H_%M_%S', time.gmtime(startTime)) + '.zip'), mode = 'a') as zf:
-					info = zipfile.ZipInfo(str(itemPath[0]) + str(itemName[0]), date_time = itemZIPModified)
-					zf.writestr(info, fakeFile.getvalue())
-				itemZIPModified = datetime.datetime.fromtimestamp(time.mktime(itemZIPModified)).strftime('%Y-%m-%d %H:%M:%S')
-			except ValueError: # triggers on the monkey-patched function
-				status = 'Skipped file'
-				reason = 'Extracted child item (Extracted archive item)'
-				itemXRYModified = ''
-				itemZIPModified = ''
-			except Exception as e: # triggers on all other items with no Date Modified, which is an indication of XRY synthesised item
-				#print(e) # if you are getting weird results (e.g., 0 files exported), uncomment this print() to see why the files fail to be exported
-				status = 'Skipped file'
-				reason = 'Extracted child item (Synthesised XRY item)'
-				itemXRYModified = ''
-				itemZIPModified = ''
-		else: # we are not interested in "files" that do not have a Path or File Name
-			status = 'Skipped file'
-			reason = 'Missing Path or Name'
-			itemZIPModified = ''
-			try:
-				itemXRYModified = str(itemXRYModified[0])
-			except:
-				itemXRYModified = ''
+		try:
+			mem = image.create_memifc(itemData[0])
+			itemBytes = io.BytesIO(mem[:])
+			itemSize = itemBytes.getbuffer().nbytes
+		except:
+			itemBytes = None
+			itemSize = 0
 		
-		try:
-			itemPath = str(itemPath[0])
-		except:
-			itemPath = ''
-		try:
-			itemName = str(itemName[0])
-		except:
-			itemName = ''
-		try:
-			itemSize = str(fakeFile.getbuffer().nbytes)
-		except:
-			itemSize = '0'
-		logText += '"' + status + '","' + reason + '","' + itemPath + '","' + itemName + '","' + itemXRYModified + '","' + itemZIPModified + '","' + itemSize + '"\n'
+		m = MemoryStatusEx()
+		windll.kernel32.GlobalMemoryStatusEx(byref(m))
+		if (m.availPhys / (1024.)**3) <= memoryMargin:
+			print('Reached the set memory margin limit (' + str(memoryMargin) + ' GB). Writing in-memory files to disk.')
+			writeToFile()
+			fileBuffer.append([itemName, itemPath, itemXRYModified, itemBytes, str(itemSize)])
+		else:
+			fileBuffer.append([itemName, itemPath, itemXRYModified, itemBytes, str(itemSize)])
 		parseRecursive(image, node, items[i])
 
 outputDir = os.path.join(os.path.expanduser('~'), 'Desktop')
+fileBuffer = []
+memoryMargin = 3 # The script will write files held in memory to disk once the computer only has this amount of RAM (in GB) available.
 startTime = time.time()
 logText = '"Status","Reason","Path","Filename","XRY Date Modified","ZIP Date Modified","Size"\n'
 
 def main(image, node):
 	volumeRoot = image.get_children(xry.nodeids.roots.volume_root)
 	parseRecursive(image, node, volumeRoot)
+	writeToFile()
 	runTime = int(round(time.time() - startTime))
-	print('Created  Extracted_' + time.strftime('%Y-%m-%d %H_%M_%S', time.gmtime(startTime)) + '.zip in ' + outputDir)
-	print('Computing statistics and generating a log file. Please stand by.')
 	with open(os.path.join(outputDir,  'Extracted_' + time.strftime('%Y-%m-%d %H_%M_%S', time.gmtime(startTime)) + '_log.csv'), 'w', encoding = 'utf8', errors = 'ignore') as lf:
 		lf.write(logText)
 	logDict = csv.DictReader(io.StringIO(logText))
@@ -143,5 +178,6 @@ def main(image, node):
 		encountered += 1
 		if r['Status'] == 'OK':
 			exported += 1
-	print(f'Processed {encountered:,d} case files. Exported {exported:,d} files in {runTime:,d} seconds.')
+	print('Created  Extracted_' + time.strftime('%Y-%m-%d %H_%M_%S', time.gmtime(startTime)) + '.zip in ' + outputDir)
+	print(f'Processed {encountered:,d} case items. Exported {exported:,d} files. Time spent ' + str(datetime.timedelta(seconds = runTime)) + '.')
 	print('Done. See log for details.')
